930886ae88e150f8319c31a1a61fc3b151cb2ba3
Parth <parsjoshi2@gmail.com>
2025-05-05 23:38:52 +0530

    loader css corrected

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,56 +9,43 @@ import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import Loading from "./loading";
 
-
-
-
-
-
-
 export default function page() {
   const [prompt, setPrompt] = useState("");
-  const {mapData,setMapData,allMaps,setAllMaps} = useContext(DashboardContext)
+  const { mapData, setMapData, allMaps, setAllMaps } =
+    useContext(DashboardContext);
   const [loading, setLoading] = useState(false);
   const router = useRouter();
-  
 
   return (
     <>
       <div className="flex-col flex-1">
-       
-          <>
-          {!loading?
+        <>
+          {!loading ? (
             <InputBox
               prompt={prompt}
               setPrompt={setPrompt}
               onClickFunction={async () => {
-                
                 setLoading(true);
                 let data = await makeMaps(prompt);
-                if(!data.success){
+                if (!data.success) {
                   setLoading(false);
                   toast.error(data.error);
 
-                  return
+                  return;
                 }
                 setAllMaps((prev: any[]) => [...prev, data]);
                 setMapData(data);
-                
-                
-                
+
                 router.push(`/dashboard/m/${data._id}`);
-                
               }}
-            />:<Loading />
-}
-
-
-            
-          </>
-      
+            />
+          ) : (
+            <div className="flex flex-1 justify-center items-center">
+              <Loading />
+            </div>
+          )}
+        </>
       </div>
     </>
   );
 }
-
-
