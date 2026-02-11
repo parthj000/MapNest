ca6d591413a0ce230dc30b8c55a6b169fe66fe3f
Parth <parsjoshi2@gmail.com>
2026-02-11 09:50:30 +0530

    credit expired ToT

--- src/app/actions/makeMaps.ts
+++ src/app/actions/makeMaps.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -6,21 +6,13 @@ import { clientPromise } from "@/lib/dbConnect";
 import UserModel from "@/models/User";
 import MapModel from "@/models/Map";
 
-
-
-
-
-
 export const makeMaps = async (prompt: string) => {
+  const session: any = await getServerSession();
+  // console.log(session);
+  // if(!session){
+  //   redirect("/signup");
+  // }
 
-    
-
-  const session:any = await getServerSession();
-  console.log(session);
-  if(!session){
-    redirect("/signup");
-  }
-  
   const geminiPrompt = `
 Generate a hierarchical JSON structure to represent a mind map of the following topic:
 "${prompt}"
@@ -34,79 +26,74 @@ Output ONLY the JSON is required no markdown or other explanation.
 `;
 
   try {
-    if(!prompt){
+    if (!prompt) {
       throw new Error("Empty prompt not allowed");
     }
 
+    await clientPromise();
+    // const user = await UserModel.findOne({ gmail: session.user.email });
+    // let credits: number = user.credits;
 
-await clientPromise();
-    const user = await UserModel.findOne({gmail:session.user.email});
-  let credits:number = user.credits;
-    
+    // credits = credits - 1;
+    // if (credits < 0) {
+    //   throw new Error("Not enough credits!");
+    // }
+    // user.credits = credits;
 
-  credits= credits-1;
-  if(credits<0){
-    throw new Error("Not enough credits!");
-      
-    }
-  user.credits = credits;
-    
     const res = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.API_KEY}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [{ text: geminiPrompt }],
+            },
+          ],
+        }),
+        cache: "no-store",
       },
-      body: JSON.stringify({
-        contents: [
-          {
-            parts: [{ text: geminiPrompt }],
-          },
-        ],
-      }),
-      cache: "no-store",
+    );
+
+    const data = await res.json();
+    console.log(data);
+
+    const inp = data.candidates[0].content.parts[0].text;
+    const match = inp
+      .replace(/^\s*```json\s*/i, "")
+      .replace(/\s*```[\s\n]*$/, "")
+      .trim();
+
+    const mapObject = {
+      mapJson: JSON.parse(match),
+      userGmail: session.user.email,
+      prompt: prompt,
+    };
+    const newMap = new MapModel(mapObject);
+
+    const h = await newMap.save().then(console.log("saved succesfully"));
+
+    await user.save();
+    return {
+      prompt: h.prompt,
+      mapJson: h.mapJson,
+      _id: h._id.toString(),
+      success: true,
+    };
+  } catch (error: any) {
+    if (error instanceof TypeError) {
+      return {
+        error:
+          "Credits of this broke developer has expired, need to upgrade his gcp :))",
+        success: false,
+      };
+    } else {
+      console.log("Some other error:", error);
     }
-  );
 
-  const data = await res.json();
-
-  
-  
-  const inp = data.candidates[0].content.parts[0].text;
-  const match = inp
-    .replace(/^\s*```json\s*/i, "")
-    .replace(/\s*```[\s\n]*$/, "")
-    .trim();
-
-const mapObject = {
-    mapJson:JSON.parse(match),
-    userGmail:session.user.email,
-    prompt:prompt
-
-
+    return { error: error.message, success: false };
   }
-    const newMap = new MapModel(mapObject)
-  
-  const h = await newMap.save().then(console.log("saved succesfully"));
-  
-  await user.save();
-  
-  
-
-  
-  return {prompt:h.prompt,mapJson:h.mapJson,_id:h._id.toString(),success:true};
- 
-  } catch (error:any) {
-
-    console.log(error);
-
-    return {error:error.message,success:false}
-
-    
-  }
-
-  
-
-  
 };
